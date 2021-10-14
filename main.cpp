c20f9a08bcdfc18126158fac551e517557dc36cb
IT21017506 <87458847+it21017506@users.noreply.github.com>
2021-10-14 06:39:14 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int n1,n2,sum;
+  std::cout<<"enter your 1st number: ";
+  std::cin>>n1;
+  std::cout<<std::endl;
+  std::cout<<"enter your 2nd number: ";
+  std::cin>>n2;
+  std::cout<<std::endl;
+  sum =n1+n2;
+  std::cout<<sum<<"="<<n1<<"+"<<n2;
+  return 0;
+
+
+
+}
